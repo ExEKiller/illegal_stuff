2f4d3506c5c38cdeec1e2efc2a10f09bebef70a0
aecy <devsnake@hotmail.com>
2020-03-19 11:55:48 +0100

    remove: docs folder

--- master.cpp
+++ master.cpp
@@ -1,3 +1,16 @@
+/*
+    class variable {
+        variable = "variable";          // Variable (be same class/var -- ex: cocaine)
+        marker = "marker_name";         // MarkerName (used in Config_Gather -- ex: "cocaine_1")
+        price = 20000;                  // Price (reduce from account -- ex: 5000)
+        pos[] = { 0, 0, 0 };            // Pos (get exact pos with: getMarkerPos "marker_name" on map editor)
+        color = "ColorYellow";          // Color (use Config Name not RGBA: https://community.bistudio.com/wiki/Arma_3_CfgMarkerColors)
+        text = "Field Name";            // Text displayed on the map
+        type = "Mil_dot";               // Type of marker (https://community.bistudio.com/wiki/cfgMarkers -- ex: Mil_dot)
+        ellipse = true;                 // Ellipse around the marker (True/false -- default: true)
+    };
+*/
+
 class armapps_illegal_stuff_cfg {
     class resources {
         class cocaine {
